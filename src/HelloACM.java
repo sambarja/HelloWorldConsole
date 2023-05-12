118b6e3a97e74a7f474a1ffedc2185956ce342f9
Samuelle  P. Barja <12211729@dlsu.edu.ph>
2023-05-12 16:16:11 +0800

    Initial commit

---
+++ src/HelloACM.java
@@ -0,0 +1,14 @@
+import acm.program.*;
+
+public class HelloACM extends ConsoleProgram {
+
+    public void run() {
+        println("Hello LBYCPEI World!");
+    }
+
+    public static void main(String[] args){
+        (new HelloACM()).start(args);
+    }
+}
+
+
